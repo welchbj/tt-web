5cf742ee64dc56851f6ea17b974c39606cb2a76e
welchbj <welch18@vt.edu>
2017-08-25 22:23:11 -0400

    Complete stylistic re-write and upgrade to tt version 0.6.2

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import os
 import sys
 
 from flask import Flask, render_template, request
-from tt import BooleanExpression as bexpr, TruthTable as ttable
-from tt.errors import GrammarError
+from tt import BooleanExpression, TruthTable
+from tt.errors import (
+    EmptyExpressionError,
+    GrammarError,
+    NoEvaluationVariationError)
 
 
 app = Flask(__name__)
@@ -23,52 +26,61 @@ def index():
 
 @app.route('/evaluate')
 def evaluate():
-    # TODO: cache results based on query params
-    expr = request.args.get('expr')
-    op = request.args.get('op')
-    context = {'expr': expr}
+    # TODO: cache expression / tables objects based on query params
 
     try:
-        if not expr:
-            raise ValueError('Empty expression')
+        expr = request.args.get('expr')
+        action = request.args.get('action')
+        context = {}
 
-        b = bexpr(expr)
+        if expr is None or action is None:
+            raise ValueError('Missing required params')
 
-        if op == 'table':
-            t = ttable(b)
+        expr = expr.strip()
+        context['expr'] = expr
+        b = BooleanExpression(expr)
 
-            # this is really inefficient
-            rows = []
-            for row in t._input_combos():
-                l = [int(val) for val in row]
-                rows.append(l)
-
-            for i, result in enumerate(t.results):
-                rows[i].append(int(result))
-
+        if action == 'table':
+            t = TruthTable(b)
             context['is_table'] = True
+            context['table'] = t
+        elif action == 'satone':
+            context['is_sat'] = True
             context['symbols'] = b.symbols
-            context['rows'] = rows
-        elif op == 'symbols':
-            context['is_symbols'] = True
+
+            sat_sol = b.sat_one()
+            context['sat_iter'] = [sat_sol] if sat_sol else []
+        elif action == 'satall':
+            context['is_sat'] = True
             context['symbols'] = b.symbols
-        elif op == 'tokens':
-            context['is_tokens'] = True
-            context['tokens'] = b.tokens
+            context['sat_iter'] = list(b.sat_all())
         else:
             raise ValueError('Invalid option')
     except ValueError:
         context['is_error'] = True
-        context['error_content'] = ['You gave us an invalid value :o']
-    except GrammarError as e:
+        context['error_content'] = [
+            'Please don\'t go out of your way to break this '
+            'already-fragile system']
+    except EmptyExpressionError:
+        context['is_error'] = True
+        context['error_content'] = ['You gave us an invalid expression!']
+    except NoEvaluationVariationError:
         context['is_error'] = True
         context['error_content'] = [
-            'Error! ' + e.message + ':',
-            e.expr_str,
-            '&nbsp;' * e.error_pos + '^']
+            'You should be able to figure that out on your own']
+    except GrammarError as e:
+        context['is_error'] = True
+        context['error_content'] = [e.message]
+        if e.error_pos is not None:
+            underlined_error_expr = ''
+            for i, char in enumerate(expr):
+                underlined_error_expr += (char if i != e.error_pos else
+                                          '<u>{}</u>'.format(char))
+            context['underlined_error_expr'] = underlined_error_expr
     except Exception as e:
         context['is_error'] = True
         context['error_content'] = ['An unexpected error occured :(']
+        raise e
 
     return render_template('result.html', **context)
 
